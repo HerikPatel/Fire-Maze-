8869971d60c13905c2a7b2811d9cc6c54612f2c4
Herik Patel <67105093+HerikPatel@users.noreply.github.com>
2021-02-04 10:58:46 -0500

    Added A* Algorithm

--- Maze.py
+++ Maze.py
@@ -1,6 +1,8 @@
 import random
 import queue
 import numpy
+import time
+import math
 dfs_row = queue.LifoQueue()  # This is to store row in the queue
 dfs_column = queue.LifoQueue()  # This is to store Column in the queue
 
@@ -34,7 +36,7 @@ def check_dimensions(row, col, dim):
     return False
 
 
-def dfs(dim):
+def dfs(dim, arraytp):
     i = 0
     j = 0
     wall_counter = 0
@@ -102,6 +104,116 @@ def dfs(dim):
     return "path not found"
 
 
-arraytp = makemaaze()
-tp = dfs(Matrix_dim)
+def Euclidean_distance(i, j, dim):
+    distance = math.sqrt((dim-1 - i)**2+(dim-1-j)**2)
+    return distance
+
+
+def Heuristic(i, j, dim, arraytp):
+    row, col = -1, -1
+    current_distance, distance = 0.0, 0.0
+    if check_dimensions(i, j+1, dim) and arraytp[i, j+1] == 1:
+        distance = Euclidean_distance(i, j+1, dim)
+        if current_distance == 0.0:
+            current_distance = distance
+            row = i
+            col = j+1
+
+        elif current_distance > distance:
+            current_distance = distance
+            row = i
+            col = j+1
+    if check_dimensions(i+1, j, dim) and arraytp[i+1, j] == 1:
+        distance = Euclidean_distance(i+1, j, dim)
+        if current_distance == 0.0:
+            current_distance = distance
+            row = i+1
+            col = j
+
+        elif current_distance > distance:
+            current_distance = distance
+            row = i+1
+            col = j
+    if check_dimensions(i, j-1, dim) and arraytp[i, j-1] == 1:
+        distance = Euclidean_distance(i, j-1, dim)
+        if current_distance == 0.0:
+            current_distance = distance
+            row = i
+            col = j-1
+
+        elif current_distance > distance:
+            current_distance = distance
+            row = i
+            col = j-1
+    if check_dimensions(i-1, j, dim) and arraytp[i-1, j] == 1:
+        distance = Euclidean_distance(i-1, j, dim)
+        if current_distance == 0.0:
+            current_distance = distance
+            row = i-1
+            col = j
+
+        elif current_distance > distance:
+            current_distance = distance
+            row = i-1
+            col = j
+
+    return row, col
+
+
+def A_star(dim, arraytp):
+    A_row = queue.LifoQueue()
+    A_column = queue.LifoQueue()
+    i, j = 0, 0
+    A_column.put(0)
+    A_row.put(0)
+    arraytp[0, 0] = 3
+    while not A_row.empty():  # The loop will run till there are elements in queue, empty queue suggest no possible solution of maze
+        i, j = Heuristic(i, j, dim, arraytp)
+        if i == -1:  # pop the element from the queue no viable path
+            arraytp[A_row.get(), A_column.get()] = 0
+            if(A_row.qsize() == 0):
+                continue
+            temp_row = A_row.get()
+            temp_col = A_column.get()
+            i = temp_row
+            j = temp_col
+            A_row.put(i)
+            A_column.put(j)
+        else:
+            A_row.put(i)
+            A_column.put(j)
+            arraytp[i, j] = 3
+
+        temp_var1 = A_row.get()
+        temp_var2 = A_column.get()
+        if temp_var1 == dim-1 and temp_var2 == dim-1:
+            return "Maze solved"
+        else:
+            A_row.put(temp_var1)
+            A_column.put(temp_var2)
+
+    return "path not found"
+
+
+start, end = 0.0, 0.0
+while True:
+    mazedfs = makemaaze()
+    print("Maze made")
+    start = time.time()
+    str = A_star(Matrix_dim, mazedfs)
+    print(str)
+    end = time.time()
+    print(end-start)
+
+
+'''
+
+maze_dfs = makemaaze()
+print("Maze made")
+start = time.time()
+tp = dfs(Matrix_dim, maze_dfs)
+end = time.time()
+
+print(end-start)
 print(tp)
+'''
