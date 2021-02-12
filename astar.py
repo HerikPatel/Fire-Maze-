0bb4e751a9477dd9b3a6033c6957db15c0009254
Herik Patel <67105093+HerikPatel@users.noreply.github.com>
2021-02-12 04:52:04 -0500

    A* algorithm Optimized

--- astar.py
+++ astar.py
@@ -2,7 +2,7 @@ import heapq
 import math
 import numpy
 import queue
-
+import time
 import random
 Matrix_dim = int(input("Enter Dimension of the Matrix: "))
 probablity = float(input("Enter Probablity: "))
@@ -11,54 +11,31 @@ dfs_row = queue.LifoQueue()
 dfs_column = queue.LifoQueue()
 
 
-class block:
-    def __init__(self, position, g, h):
-        self.parent = None
-        self.position = position
-        self.g = g
-        self.h = h
-        self.f = self.g+self.h
-
-    def __lt__(self, other):
-        return self.f < other.f
-
-
-def dfs(dim, arraytp):
-    i, j = 0, 0
-    wall_counter = 0
-    dfs_column.put(0)
+def dfs(dim, arraytp, i, j):
+    dfs_row = queue.LifoQueue()  # This is to store row in the queue
+    dfs_column = queue.LifoQueue()  # This is to store Column in the queue
     counter_step = 0
-    dfs_row.put(0)
-    arraytp[0, 0] = 3
-    # if arraytp[0, 1] == 1:  # initialize the priority queue by storing values of of rows and column no blocked by wall
-    #    dfs_row.put(0)
-    #    dfs_column.put(1)
-    # if arraytp[1, 0] == 1:
-    #    dfs_row.put(1)
-    #    dfs_column.put(0)
-    # we only go back to prev node when e find that 3 sides are locked
-    while not dfs_row.empty():  # we need a check to see if last coordinate int the queue to see that we donot go back to the same path
+    wall_counter = 0
+    dfs_column.put(j)
+    dfs_row.put(i)
+    arraytp[i, j] = 3
+    while not dfs_row.empty():  # The loop will run till there are elements in queue, empty queue suggest no possible solution of maze
         counter_step = counter_step+1
-        if wall_counter == 4:
+        if wall_counter == 4:  # Value 4 means we have exhasuted all possible unexplored paths for particular cell and we remove this cell from the queue
             arraytp[dfs_row.get(), dfs_column.get()] = 0
             if(dfs_column.qsize() == 0):
                 continue
             temp_row = dfs_row.get()
             temp_col = dfs_column.get()
-            i = temp_row
-            j = temp_col
+            i, j = temp_row, temp_col
             dfs_row.put(i)
             dfs_column.put(j)
         wall_counter = 0
-        # needs to complete the stack error where the queue is not getting empty and problems in last index
-        # needed modification nned to see prev state visited to avoid infinite loop check in the if condiotion before transition if the prev state is not the same as current state
         temp_var2 = dfs_column.get()
         temp_var1 = dfs_row.get()
-        # i = temp_var1
-        # j = temp_var2
         if temp_var1 == dim-1 and temp_var2 == dim-1:
             print(dfs_row.qsize())
-            return "Yes! Successful"
+            return "Maze solved"
         else:
             dfs_row.put(temp_var1)
             dfs_column.put(temp_var2)
@@ -100,8 +77,20 @@ def dfs(dim, arraytp):
             continue
         else:
             wall_counter = wall_counter+1
-
+    # print(counter_step)
     return "path not found"
+
+
+class block:
+    def __init__(self, position, g, h):
+        self.parent = None
+        self.position = position
+        self.g = g
+        self.h = h
+        self.f = self.g+self.h
+
+    def __lt__(self, other):
+        return self.f < other.f
 
 
 def check_dimensions(row, col, dim):
@@ -109,6 +98,16 @@ def check_dimensions(row, col, dim):
         if row < dim and col < dim:
             return True
     return False
+
+
+def check_dup_openlist(current_node, node, open_list):
+    for x in open_list:
+        if x.position == node.position:
+            if x.f > node.f:
+                x = node
+
+            return open_list, False
+    return open_list, True
 
 
 def a_star(maze, row, col, dim):
@@ -125,6 +124,7 @@ def a_star(maze, row, col, dim):
         row = current_node.position[0]
         col = current_node.position[1]
         maze[row][col] = 3
+        # print(row, col)
         if row == dim-1 and col == dim-1:
             row = current_node.position[0]
             col = current_node.position[1]
@@ -141,28 +141,36 @@ def a_star(maze, row, col, dim):
                 (dim-1 - row+1)**2+(dim-1-col)**2)
             node = block([row+1, col], current_node.g+1, h)
             node.parent = current_node
-            open_list.append(node)
+            open_list, bol = check_dup_openlist(current_node, node, open_list)
+            if bol:
+                open_list.append(node)
 
         if check_dimensions(row, col+1, dim) and maze[row, col+1] == 1:
             h = math.sqrt(
                 (dim-1 - row)**2+(dim-1-col+1)**2)
             node = block([row, col+1], current_node.g+1, h)
             node.parent = current_node
-            open_list.append(node)
+            open_list, bol = check_dup_openlist(current_node, node, open_list)
+            if bol:
+                open_list.append(node)
 
         if check_dimensions(row, col-1, dim) and maze[row, col-1] == 1:
             h = math.sqrt(
                 (dim-1 - row)**2+(dim-1-col-1)**2)
             node = block([row, col-1], current_node.g+1, h)
             node.parent = current_node
-            open_list.append(node)
+            open_list, bol = check_dup_openlist(current_node, node, open_list)
+            if bol:
+                open_list.append(node)
 
         if check_dimensions(row-1, col, dim) and maze[row-1, col] == 1:
             h = math.sqrt(
                 (dim-1 - row-1)**2+(dim-1-col)**2)
             node = block([row-1, col], current_node.g+1, h)
             node.parent = current_node
-            open_list.append(node)
+            open_list, bol = check_dup_openlist(current_node, node, open_list)
+            if bol:
+                open_list.append(node)
 
         close_list.append(current_node)
     return path
@@ -191,12 +199,21 @@ def makemaaze():
 while True:
     maze = makemaaze()
     maze2 = numpy.copy(maze)
-   #str = dfs(Matrix_dim, maze2)
+    print("Dfs")
+    str = dfs(Matrix_dim, maze2, 0, 0)
+   # print(maze)
+    print("A Star")
+    start = time.time()
     print(maze)
     print(len(a_star(maze, 0, 0, Matrix_dim))+1)
-
+    end = time.time()
 
 '''
+
+
+maze2 = numpy.array(maze)
+print(len(a_star(maze2, 0, 0, 20))+1)
+
 tp = block([3, 2], 13, 2)
 
 path = []
@@ -207,4 +224,103 @@ heapq.heapify(path)
 tpp = heapq.heappop(path)
 print(tpp.g)
 print(tpp.h)
+'''
+''' Backup Astar
+def Euclidean_distance(i, j, dim):
+    distance = math.sqrt((dim-1 - i)**2+(dim-1-j)**2)
+    return distance
+
+
+def Heuristic(i, j, dim, arraytp, current_cost):
+    row, col = -1, -1
+    prev_cost = current_cost.get()
+    current_cost.put(prev_cost)
+    current_distance, distance = 0.0, 0.0
+    if check_dimensions(i, j+1, dim) and arraytp[i, j+1] == 1:
+        distance = Euclidean_distance(i, j+1, dim)
+        distance = distance+prev_cost+1
+        if current_distance == 0.0:
+            current_distance = distance
+            row, col = i, j+1
+
+        elif current_distance > distance:
+            current_distance = distance
+            row, col = i, j+1
+
+    if check_dimensions(i+1, j, dim) and arraytp[i+1, j] == 1:
+        distance = Euclidean_distance(i+1, j, dim)
+        distance = distance+prev_cost+1
+        if current_distance == 0.0:
+            current_distance = distance
+            row, col = i+1, j
+
+        elif current_distance > distance:
+            current_distance = distance
+            row, col = i+1, j
+
+    if check_dimensions(i, j-1, dim) and arraytp[i, j-1] == 1:
+        distance = Euclidean_distance(i, j-1, dim)
+        distance = distance+prev_cost+1
+        if current_distance == 0.0:
+            current_distance = distance
+            row, col = i, j-1
+
+        elif current_distance > distance:
+            current_distance = distance
+            row, col = i, j-1
+
+    if check_dimensions(i-1, j, dim) and arraytp[i-1, j] == 1:
+        distance = Euclidean_distance(i-1, j, dim)
+        distance = distance+prev_cost+1
+        if current_distance == 0.0:
+            current_distance = distance
+            row, col = i-1, j
+
+        elif current_distance > distance:
+            current_distance = distance
+            row, col = i-1, j
+
+    return row, col
+
+
+def A_star(dim, arraytp, i, j):  # arraytp is the mzae
+    nodes_visited = 0
+    current_cost = queue.LifoQueue()
+    current_cost.put(0)
+    A_row, A_column = queue.LifoQueue(), queue.LifoQueue()
+    A_column.put(j)
+    A_row.put(i)
+    arraytp[i, j] = 3
+    while not A_row.empty():  # The loop will run till there are elements in queue, empty queue suggest no possible solution of maze
+        i, j = Heuristic(i, j, dim, arraytp, current_cost)
+        if i == -1:  # pop the element from the queue no viable path
+            arraytp[A_row.get(), A_column.get()] = 0
+            if(A_row.qsize() == 0):
+                continue
+            temp_row = A_row.get()
+            temp_col = A_column.get()
+            current_cost.get()
+            i, j = temp_row, temp_col
+            A_row.put(i)
+            A_column.put(j)
+            # current_cost.put(temp_cost)
+        else:
+            temp_cost = current_cost.get()
+            current_cost.put(temp_cost)
+            current_cost.put(temp_cost+1)
+            A_row.put(i)
+            A_column.put(j)
+            arraytp[i, j] = 3
+            nodes_visited = nodes_visited+1
+
+        temp_var1 = A_row.get()
+        temp_var2 = A_column.get()
+        if temp_var1 == dim-1 and temp_var2 == dim-1:
+            print(nodes_visited)
+            return "Maze solved"
+        else:
+            A_row.put(temp_var1)
+            A_column.put(temp_var2)
+
+    return "path not found"
 '''
